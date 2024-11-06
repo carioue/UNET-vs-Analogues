cfda5f2100ab4083b330c432dce17f8a37bc8c34
carioue <enora.cariou@meteo.fr>
2024-11-06 09:43:29 +0100

    Update UNET2_test_UNET.py

--- UNET2_test_UNET.py
+++ UNET2_test_UNET.py
@@ -11,7 +11,7 @@ slp_test = xr.open_dataset('/UNET-vs-Analogues_data/psl_day_MIROC6_unet_r5i1p1f1
 T_target_test = xr.open_dataset('/UNET-vs-Analogues_data/anomalies_r5.nc').tasano[25550:25550+26645] #get corresponding temperature anomalies from 1950 to 2022
 
 
-UNET = load_model('/model_40members_input3286') #load the UNET already trained
+UNET = load_model('model_40members_input3286') #load the UNET already trained
 T_pred = UNET.predict(slp_test) #apply the UNET on testing data
 T_pred = T_pred.reshape((T_pred.shape[0],T_pred.shape[1],T_pred.shape[2])) 
 
